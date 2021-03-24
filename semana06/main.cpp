6252121305e1b7bbedf77acbafe1fc0f47876b66
Samuel Abreu <samuelapp@outlook.com>
2021-03-24 00:11:16 -0300

    .

--- semana06/main.cpp
+++ semana06/main.cpp
@@ -15,52 +15,47 @@ int main(int argc, const char** argv) {
     img = imread(argv[1]);
     Mat grayImg;
     img.copyTo(blurredImg);
-    // cvtColor(img, grayImg, COLOR_BGR2GRAY);
+    cvtColor(img, grayImg, COLOR_BGR2GRAY);
+    Mat grayBlurredImg;
+    grayImg.copyTo(grayBlurredImg);
     // grayImg.copyTo(blurredImg);
-    Mat kernel = (Mat_<uchar>(3, 3) << 0, 1, 0,
-                                       0, 1, 0,
-                                       0, 1, 0);
-    int kernelSum = 18;
-    // Mat sample = Mat_<Vec3f>(3,3);
-    // for(int i = 1; i < img.rows-1; i++){
-        // for(int j = 1; j < img.cols-1; j++){
-            // sample.at<Vec3f>(0 , 0) = (Vec3f)img.at<Vec3b>(i-1, j-1);
-            // sample.at<Vec3f>(0 , 1) = (Vec3f)img.at<Vec3b>(i-1, j);
-            // sample.at<Vec3f>(0 , 2) = (Vec3f)img.at<Vec3b>(i-1, j+1);
-            // sample.at<Vec3f>(1 , 0) = (Vec3f)img.at<Vec3b>(i,   j-1);
-            // sample.at<Vec3f>(1 , 1) =(Vec3f) img.at<Vec3b>(i,   j);
-            // sample.at<Vec3f>(1 , 2) =(Vec3f) img.at<Vec3b>(i,   j+1);
-            // sample.at<Vec3f>(2 , 0) =(Vec3f) img.at<Vec3b>(i+1, j-1);
-            // sample.at<Vec3f>(2 , 1) =(Vec3f) img.at<Vec3b>(i+1, j);
-            // sample.at<Vec3f>(2 , 2) =(Vec3f) img.at<Vec3b>(i+1, j+1);
-            // // blurredImg.at<Vec3b>(i,j) = (Vec3b)sample.dot(kernel)*1/16;
-            // //cout << sample.dot(kernel)*1/16 << endl;
-            // for(int k = 0; k < 3; k++)
-                // for(int l = 0; l < 3; l++){
-                    //
-//
-                // }
-        // }
-//
-    // }
+    Mat kernel = (Mat_<uchar>(3, 3) << 1, 1, 1,
+                                       1, 1, 1,
+                                       1, 1, 1);
+    int kernelSum = 9;
 
     for(int i = 1; i < img.rows-1; i++){
         for(int j = 1; j < img.cols-1; j++){
-            // if(i == 0 || j == 0 || j == img.rows -1 || i == img.cols -1)
-                // continue;
             Vec3i value = ((Vec3i)img.at<Vec3b>(i,j)*kernel.at<uchar>(1,1)
                         + kernel.at<uchar>(0,1)*(Vec3i)img.at<Vec3b>(i-1, j) + kernel.at<uchar>(2,1)*(Vec3i)img.at<Vec3b>(i+1, j) + kernel.at<uchar>(1,0)*(Vec3i)img.at<Vec3b>(i, j-1) + kernel.at<uchar>(1,2)*(Vec3i)img.at<Vec3b>(i, j+1)
                         + kernel.at<uchar>(0,0)*(Vec3i)img.at<Vec3b>(i-1, j-1) +kernel.at<uchar>(0,2)*(Vec3i)img.at<Vec3b>(i-1, j+1) +kernel.at<uchar>(2,0)*(Vec3i)img.at<Vec3b>(i+1, j-1) +kernel.at<uchar>(2,2)*(Vec3i)img.at<Vec3b>(i+1, j+1))
-                        *(1/(float)16);
-            cout << value[0] << ", " << value[1] << ", " << value[2] << endl;
+                        *(1/(float)kernelSum);
             Vec3b val = (Vec3b)value;
             blurredImg.at<Vec3b>(i,j) = val;
         }
     }
-    resize(blurredImg, blurredImg, Size(0,0), 2, 2);
-    resize(img, img, Size(0,0), 2, 2);
+    // para imagens em preto e branco:
+    for(int i = 1; i < grayImg.rows-1; i++){
+        for(int j = 1; j < grayImg.cols-1; j++){
+            int value = (int)(grayImg.at<uchar>(i,j)*(int)kernel.at<uchar>(1,1)
+                        + kernel.at<uchar>(0,1)*(int)grayImg.at<uchar>(i-1, j)
+                        + kernel.at<uchar>(2,1)*(int)grayImg.at<uchar>(i+1, j)
+                        + kernel.at<uchar>(1,0)*(int)grayImg.at<uchar>(i, j-1)
+                        + kernel.at<uchar>(1,2)*(int)grayImg.at<uchar>(i, j+1)
+                        + kernel.at<uchar>(0,0)*(int)grayImg.at<uchar>(i-1, j-1)
+                        + kernel.at<uchar>(0,2)*(int)grayImg.at<uchar>(i-1, j+1)
+                        + kernel.at<uchar>(2,0)*(int)grayImg.at<uchar>(i+1, j-1)
+                        + kernel.at<uchar>(2,2)*(int)grayImg.at<uchar>(i+1, j+1))
+                        *(1/(float)kernelSum); // nao me orgulho dessas 9 linhas
+            uchar val = (uchar)value;
+            grayBlurredImg.at<uchar>(i,j) = val;
+        }
+    }
     imshow("vai1", img);
     imshow("vai", blurredImg);
+    imshow("vai2", grayBlurredImg);
+    imshow("vai3", grayImg);
+
     waitKey(0);
     return 0;
 }
